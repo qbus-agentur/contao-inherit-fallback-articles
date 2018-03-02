6de033044bf0e69a620ecd710347308b17b1d885
Alex Wuttke <alw@qbus.de>
2018-03-02 16:00:38 +0100

    Add language files.

---
+++ languages/de/fallback_articles_methods.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Inherit Fallback Articles Extension by Qbus
+ *
+ * @author  Alex Wuttke <alw@qbus.de>
+ * @license LGPL-3.0+
+ */
+
+$GLOBALS['TL_LANG']['fallback_articles_methods']['inherit'] = 'Erben';
